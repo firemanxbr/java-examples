e503e522af42d49bfa3d4a1f764adb3dba89f231
Marcelo Barbosa <marcelo.barbosa@datacom.ind.br>
2015-09-28 10:44:03 -0300

    first examples for guide: https://www.caelum.com.br/apostila-java-orientacao-objetos/

---
+++ 3.1.AgeTest.java
@@ -0,0 +1,15 @@
+class AgeTest {
+
+	public static void main(String[] args) {
+	// print age
+	int age = 20;
+	System.out.println(age);
+
+	// generate the age for future
+	int ageForFuture;
+	ageForFuture = age + 1;
+
+    // print future age
+	System.out.println(ageForFuture);
+	}
+}
